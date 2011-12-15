f11d1c0e65d3d8eafa929dbd0aaa92af4433f48d
Zian Fanti <zian@Mac-Pro-Imagenes.local>
2011-12-15 13:18:15 -0600

    create a slider for show image stacks

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,65 +9,65 @@
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
-  ui->setupUi(this);
-
-  this->setAttribute(Qt::WA_DeleteOnClose);
-
-  this->displayWidget = new QVTKImageWidget();
-
-  QVBoxLayout *layout = new QVBoxLayout;
-  layout->setContentsMargins(0, 0, 0, 0);
-  layout->setSpacing(0);
-  layout->QLayout::addWidget(displayWidget);
-  ui->imageWidget->setLayout(layout);
-
+    ui->setupUi(this);
+    
+    this->setAttribute(Qt::WA_DeleteOnClose);
+    
+    this->displayWidget = new QVTKImageWidget();
+    
+    QVBoxLayout *layout = new QVBoxLayout;
+    layout->setContentsMargins(0, 0, 0, 0);
+    layout->setSpacing(0);
+    layout->QLayout::addWidget(displayWidget);
+    ui->imageWidget->setLayout(layout);
+    
+    // hide the slider andonly show if an image stack is load
+    ui->imageSlider->hide();
 }
 
 
 MainWindow::~MainWindow()
 {
-  delete ui;
+    delete ui;
 }
 
 
 void
 MainWindow::addImages()
 {
-
-  //    this->imagePath = QFileDialog::getExistingDirectory(); 
-  //    addLogText("Set image path: <b>" + imagePath + "</b>");   
-  //    printf("Image Path: %s \n", imagePath.toAscii().data());
-
-  this->imagesFilenames = QFileDialog::getOpenFileNames(this, tr("Open Images"), QDir::currentPath());
-  if (!imagesFilenames.isEmpty())
+    
+    //    this->imagePath = QFileDialog::getExistingDirectory(); 
+    //    addLogText("Set image path: <b>" + imagePath + "</b>");   
+    //    printf("Image Path: %s \n", imagePath.toAscii().data());
+    
+    this->imagesFilenames = QFileDialog::getOpenFileNames(this, tr("Open Images"), QDir::currentPath());
+    if (!imagesFilenames.isEmpty())
     {
-      if (imagesFilenames.size() == 1)
+        if (imagesFilenames.size() == 1)
         {
-          addLogText("Loading: <b>" + imagesFilenames.first() + "</b>");
-          displayWidget->setAndDisplayImage(imagesFilenames.first());
-
-          addLogText("Image Type: <b>" + displayWidget->getImageType() + "</b>");
-          addLogText("Pixel Type: <b>" + displayWidget->getPixelType() + "</b>");
-          addLogText("Num of Dimensions: <b>" + displayWidget->getNumOfDimesions() + "</b>");
+            addLogText("Loading: <b>" + imagesFilenames.first() + "</b>");
+            displayWidget->setAndDisplayImage(imagesFilenames.first());
+            
+            addLogText("Image Type: <b>" + displayWidget->getImageType() + "</b>");
+            addLogText("Pixel Type: <b>" + displayWidget->getPixelType() + "</b>");
+            addLogText("Num of Dimensions: <b>" + displayWidget->getNumOfDimesions() + "</b>");
         }
-      else
+        else
         {
-          // if selected multiple files
-          for (int i = 0; i < imagesFilenames.size(); i++)
-            {
-              addLogText("Loading: <b>" + imagesFilenames.at(i) + "</b>");
-              
-
-            }
-
+            // if selected multiple files
+            displayWidget->setAndDisplayMultipleImages(imagesFilenames);             
+            for (int i = 0; i < imagesFilenames.size(); i++)
+            {               
+                addLogText("Loading: <b>" + imagesFilenames.at(i) + "</b>");
+            }            
         }
     }
-  else
+    else
     {
-      QErrorMessage errorMessage;
-      errorMessage.showMessage("No file specified for loading");
-      errorMessage.exec();
-      return;
+        QErrorMessage errorMessage;
+        errorMessage.showMessage("No file specified for loading");
+        errorMessage.exec();
+        return;
     }
 }
 
@@ -75,17 +75,17 @@ MainWindow::addImages()
 void
 MainWindow::probeCalibration()
 {
-  std::cout << "probe calibration" << std::endl;
+    std::cout << "probe calibration" << std::endl;
 }
 
 
 void
 MainWindow::addLogText(QString str)
 {
-  ui->textEdit->append(str);
-
-  // move the cursor to the end of the last line
-  QTextCursor cursor = ui->textEdit->textCursor();
-  cursor.movePosition(QTextCursor::End);
-  ui->textEdit->setTextCursor(cursor);
+    ui->textEdit->append(str);
+    
+    // move the cursor to the end of the last line
+    QTextCursor cursor = ui->textEdit->textCursor();
+    cursor.movePosition(QTextCursor::End);
+    ui->textEdit->setTextCursor(cursor);
 }
