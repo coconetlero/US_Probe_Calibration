f430c5c286bee7fa8cbd27b0e51465e3e31aa691
Zian Fanti <zian@Mac-Pro-Imagenes.local>
2011-12-16 17:28:25 -0600

    implement one type of crop in the crop slot

--- ProbeCalibrationDialog.cpp
+++ ProbeCalibrationDialog.cpp
@@ -7,20 +7,49 @@
 //
 #include <QErrorMessage>
 
+#include <vtkExtractVOI.h>
+#include <vtkImageCast.h>
+
 #include "ProbeCalibrationDialog.h"
+#include "QVTKImageWidget.h"
 
 ProbeCalibrationDialog::ProbeCalibrationDialog(QWidget* parent) : QDialog(parent)
 {
     ui.setupUi(this);
 }
 
-ProbeCalibrationDialog::~ProbeCalibrationDialog() {}
+ProbeCalibrationDialog::~ProbeCalibrationDialog() 
+{
+    this->image = NULL;    
+}
 
 
 void ProbeCalibrationDialog::crop()
 {
+//    int* dim = this->currentImage->GetDimensions();
+//    std::cout << "dims: " << dim[0] << ", " << dim[1] << std::endl;
+    
+    
+    vtkSmartPointer<vtkExtractVOI> extractVOI = vtkSmartPointer<vtkExtractVOI>::New();
     if (ui.radioButton_4->isChecked()) {
-        
+        if (workWithStack) {
+            for (uint i = 0; i < imageStack.size(); i++) {
+                
+            }
+        }
+        else {
+            extractVOI->SetInput(image);
+            extractVOI->SetVOI(91, 478, 49, 446, 0, 0);
+            extractVOI->Update();
+            currentImage = extractVOI->GetOutput();
+            
+            vtkSmartPointer<vtkImageCast> cropCastFilter = vtkSmartPointer<vtkImageCast>::New();
+            cropCastFilter->SetInputConnection(currentImage->GetProducerPort());
+            cropCastFilter->SetOutputScalarTypeToUnsignedChar();
+            cropCastFilter->Update();
+            
+            ((QVTKImageWidget) parentWidget()).setAndDisplayImage(cropCastFilter->GetOutput());                        
+        }
     }
     else if (ui.radioButton_5->isChecked()) {
         
@@ -36,6 +65,24 @@ void ProbeCalibrationDialog::crop()
         errorMessage.showMessage("<b>No Depth selected</b> <br /> Select one depth before crop");
         errorMessage.exec();
         return;
-    }
+    }        
 }
+
+
+void ProbeCalibrationDialog::setImage(vtkSmartPointer<vtkImageData> image) 
+{
+    this->workWithStack = false;
+    this->image = image;
+    this->currentImage = image;
+}
+
+
+void ProbeCalibrationDialog::
+setImageStack(std::vector<vtkSmartPointer<vtkImageData> > imageStack)
+{
+    this->workWithStack = true;
+    this->imageStack = imageStack;
+    this->currentImage = imageStack.at(0);
+}
+
 
--- ProbeCalibrationDialog.h
+++ ProbeCalibrationDialog.h
@@ -10,6 +10,8 @@
 #define US_Probe_Calibration_ProbeCalibrationDialog_h
 
 #include <QDialog>
+#include <vtkSmartPointer.h>
+#include <vtkImageData.h>
 
 #include "ui_ProbeCalibrationDialog.h"
 
@@ -22,11 +24,34 @@ public:
     ProbeCalibrationDialog(QWidget* parent = 0);    
     virtual ~ProbeCalibrationDialog();
     
-
+    /**
+     * \brief Set this stack of vtkImageData 
+     * \param[in] a std Vector of vtkImageData
+     */
+    void setImageStack(std::vector< vtkSmartPointer<vtkImageData> > imageStack);
+    
+    /**
+     * \brief Set this vtkImageData 
+     * \param[in] a smart Pointer of vtkImageData
+     */
+    void setImage(vtkSmartPointer<vtkImageData> image);
+    
     
 private:
     
     Ui::ProbeCalibrationDialog ui;
+    
+    bool workWithStack;
+    
+    /** \brief the vtkImageData to work */
+    vtkSmartPointer<vtkImageData> image;
+    
+    /** \brif a copy of one image */
+    vtkSmartPointer<vtkImageData> currentImage;
+    
+    /** \brief an Array of vtkImageData to work */
+    std::vector< vtkSmartPointer<vtkImageData> > imageStack;
+    
     
 private slots:
     /**
