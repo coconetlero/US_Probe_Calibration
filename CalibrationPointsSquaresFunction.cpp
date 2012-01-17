7afd502a2cc4c3033069b9060fe6ba34b5334823
Zian Fanti <zian@Mac-Pro-Imagenes.local>
2012-01-16 20:14:25 -0600

    create the implementation of the last square function

---
+++ CalibrationPointsSquaresFunction.cpp
@@ -0,0 +1,12 @@
+//
+//  CalibrationPointsSquaresFunction.cpp
+//  US_Probe_Calibration
+//
+//  Created by Zian Fanti on 16/01/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include "CalibrationPointsSquaresFunction.h"
+
+
+
---
+++ CalibrationPointsSquaresFunction.h
@@ -0,0 +1,26 @@
+//
+//  CalibrationPointsSquaresFunction.h
+//  US_Probe_Calibration
+//
+//  Created by Zian Fanti on 16/01/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef US_Probe_Calibration_CalibrationPointsSquaresFunction_h
+#define US_Probe_Calibration_CalibrationPointsSquaresFunction_h
+
+#include <vnl/vnl_least_squares_function.h>
+#include <vnl/vnl_matrix.h>
+#include <vnl/vnl_vector.h>
+
+class CalibrationPointsSquaresFunction : public vnl_least_squares_function {
+    
+public:
+    
+    CalibrationPointsSquaresFunction(vnl_vector< vnl_matrix<double> > &transformationSet);
+    
+    virtual void f( vnl_vector<double> const &x, vnl_vector<double> &fx);
+
+};
+
+#endif
